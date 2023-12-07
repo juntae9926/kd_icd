ccd7160fd5d00a637fa9bc42e09e9de49ef3a667
juntae9926 <deluxe9926@gmail.com>
2023-12-08 02:29:09 +0900

    update

--- sscd/disc_eval.sh
+++ sscd/disc_eval.sh
@@ -5,5 +5,4 @@ sscd/disc_eval.py --disc_path /dataset/DISC --gpus=2 \
   --workers=28 \
   --backbone TV_EFFICIENTNET_B0 \
   --dims 512 \
-  --features disc_eval/efficientnet/predictions.pt
-#   --student /user/weights/efficientnet.ckpt
+  --student /user/weights/efficientnet.ckpt
--- sscd/train.sh
+++ sscd/train.sh
@@ -1,9 +1,9 @@
-MASTER_ADDR="localhost" MASTER_PORT="20281" NODE_RANK="0" WORLD_SIZE=2 \
+MASTER_ADDR="localhost" MASTER_PORT="20000" NODE_RANK="0" WORLD_SIZE=2 \
   ./sscd/train.py --nodes=1 --gpus=2 \
-  --train_dataset_path /dataset/DISC/images/dev_queries \
-  --val_dataset_path /dataset/DISC/val_images \
-  --query_dataset_path /dataset/DISC/images/final_queries \
-  --ref_dataset_path /dataset/DISC/images/references \
+  --train_dataset_path {YOUR_TRAIN_PATH} \
+  --val_dataset_path {YOUR_VALIDATION_PATH} \
+  --query_dataset_path {YOUR_QUERY_PATH} \
+  --ref_dataset_path {YOUR_REFERENCE_PATH} \
   --augmentations=ADVANCED --mixup=true \
   --batch_size 256 \
   --workers 16 \
